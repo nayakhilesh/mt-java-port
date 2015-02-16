5a8f7bb9619df7d5b1d147ef8ab7db39f3105d55
Akhilesh Nayak <nayakhilesh@gmail.com>
2015-02-16 16:54:02 -0500

    renamed corpus files

--- src/main/java/com/github/nayakhilesh/Main.java
+++ src/main/java/com/github/nayakhilesh/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        //usage: corpus.en, corpus.es
+        //usage: corpus.en.txt corpus.es.txt
 
         long start = System.currentTimeMillis();
 
